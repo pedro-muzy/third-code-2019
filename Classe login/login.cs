ad9e4f5a5805cfdafd351fdd55b6f0ea711d0635
Pedro Muzy <dropeezymu@gmail.com>
2019-08-30 11:14:32 -0300

    Create login.cs

---
+++ Classe login/login.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_projeto
+{
+    class login
+    {
+        private string endereco_email;
+        private string senha;
+
+        public login()
+        {
+
+        }
+
+        public string getEmail(string email)
+        {
+            endereco_email = email;
+            return (endereco_email);
+        }
+
+        public string getSenha(String s)
+        {
+            senha = s;
+            return (senha);
+        }
+
+        public bool ValidarEmail(String email)
+        {
+            bool emailValido = false;
+
+            //Expressão regular retirada de
+            //https://msdn.microsoft.com/pt-br/library/01escwtf(v=vs.110).aspx
+            string emailRegex = string.Format("{0}{1}",
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))",
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
+
+            try
+            {
+                emailValido = Regex.IsMatch(
+                    email,
+                    emailRegex);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                emailValido = false;
+            }  
+
+            return emailValido;
+
+        }   
+
+    }
+}
